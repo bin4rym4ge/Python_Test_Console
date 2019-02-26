5b6a9334e2b41dcdff4bd59f1538899100c76616
bin4rym4ge <bin4rym4ge@users.noreply.github.com>
2019-02-26 00:10:19 -0800

    new command
    pythag

--- console/commands.py
+++ console/commands.py
@@ -2,7 +2,31 @@ class commands:
 	def test():
 		print("This is a test.")
 
+	def pythag(a,b,c):
+
+		if a and b and c:
+			print("please input only 2 values")
+
+		elif a and b:
+			c = (a**2) + (b**2)
+			return sqrt(c)
+
+		elif b and c:
+			a = (c**2) - (b**2)
+			return sqrt(a)
+
+		elif a and c:
+			b = (c**2) - (a**2)
+			return sqrt(b)
+
+		elif a or b or c:
+			print("please input more than 1 value")
+
+		else:
+			print("Error no input")
+
 
 cmds = {
-"test": commands.test
+"test": commands.test,
+"pythag": commands.pythag
 }
