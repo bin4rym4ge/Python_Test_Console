daf1f2395691b29b76afa56f1e331421c3f4080e
bin4rym4ge <bin4rym4ge@users.noreply.github.com>
2019-02-25 21:34:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ print("* Test Console *")
 print("****************")
 
 while True:
-	cmd = input("\n~~>")
-	if cmd == "":
-		break
-	interpret(cmd)
+	try:
+		cmd = input("\n~~>")
+		if cmd == "":
+			break
+		interpret(cmd)
+	except:
+		print("error")
